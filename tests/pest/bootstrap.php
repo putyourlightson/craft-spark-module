af38a340816dd7a2b070bfc4515998561d9c7be0
Ben Croker <ben@putyourlightson.com>
2024-11-05 21:48:21 +0100

    Revert

--- tests/pest/bootstrap.php
+++ tests/pest/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-define('PROJECT_VENDOR_DIR', getenv('PROJECT_VENDOR_DIR') ?: dirname(__DIR__, 2) . '/vendor');
+define('PROJECT_VENDOR_DIR', getenv('PROJECT_VENDOR_DIR') ?: dirname(__DIR__) . '/vendor');
 
 require realpath(PROJECT_VENDOR_DIR . '/autoload.php');
