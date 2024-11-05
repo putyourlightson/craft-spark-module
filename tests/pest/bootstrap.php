72c1c508f953ac708c802a2ebfe08ef59df8478b
Ben Croker <ben@putyourlightson.com>
2024-11-05 22:19:35 +0100

    Manually require the Craft class

--- tests/pest/bootstrap.php
+++ tests/pest/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 
-define('PROJECT_VENDOR_DIR', getenv('PROJECT_VENDOR_DIR') ?: dirname(__DIR__) . '/vendor');
+define('PROJECT_VENDOR_DIR', getenv('PROJECT_VENDOR_DIR') ?: dirname(__DIR__, 2) . '/vendor');
 
 require realpath(PROJECT_VENDOR_DIR . '/autoload.php');
+
+// Manually require the Craft class
+require realpath(PROJECT_VENDOR_DIR . '/craftcms/cms/src/Craft.php');
